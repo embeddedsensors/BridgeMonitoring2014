54790d1f8d0baa9cd292a8e3147698056951df5c
Matthew Iannucci <mpiannucci@gmail.com>
2014-04-08 22:23:50 -0400

    Object orient ALLL the things!!

--- src/quickSample.py
+++ src/quickSample.py
@@ -13,73 +13,85 @@ import time
 from libs.Adafruit_ADS1x15 import ADS1x15
 
 ADS1115 = 0x01  # 16-bit ADC Address
-ADC_ADDRESS_1 = 0x48
+ADC_ADDRESS_1 = 0x48  # Default, when the right pin is grounded
 
-SAMPLE_RATE = 100
-DURATION = 60
-LOGFILE_NAME = 'rawData'
-CHANNELS = [0, 1, 2]
+SAMPLE_RATE = 100  # in herz
+SAMPLE_DURATION = 60  # in seconds
+TEST_DURATION = 60*15  # in seconds
+LOGFILE_NAME = 'rawData'  # without the extension
+CHANNELS = [0, 1, 2]  # ADC channels to read in on
 
 def signal_handler(signal, frame):
-    '''
-    Handle a keyboard interrupt
+    '''Handle a keyboard interrupt
     '''
     print 'You pressed Ctrl+C! Exiting...\n'
     sys.exit(0)
 
-def sampleRun(duration, sampleRate, channels):
-    '''
-    Sample from the ADC and log into an array
-    '''
-    # Initialise the ADC using the default mode
-    # For now only use one ADC, so only create one ADC object
-    print 'Data collection started...Press Ctrl+C to exit\n'
-    adc = ADS1x15(address=ADC_ADDRESS_1, ic=ADS1115)
-    samples = range(0, ((duration*sampleRate)-1))
-    volts = [None] * len(samples)
-    for sample in samples:
-        if not isinstance(channels, int):
-            volts[sample] = [adc.readADCSingleEnded(channels[0], 3300, sampleRate) / 1000,
-                             adc.readADCSingleEnded(channels[1], 3300, sampleRate) / 1000,
-                             adc.readADCSingleEnded(channels[2], 3300, sampleRate) / 1000]
-        else:
-            volts[sample] = adc.readADCSingleEnded(channels, 3300, SAMPLE_RATE) / 1000
-    return volts
 
-def log(filename, data, sampleRate, duration, channels, rectime):
-    '''
-    Log the data collected in an array to a file to be used for later
-    '''
-    fp = open(filename + '-' + str(rectime) + '.txt', 'w+')
-    fp.write('ADS 1115 Data Collection\nSample Rate: ' + str(sampleRate) + ' Herz\nDuration: ' + str(duration) + ' Seconds\n')
-    fp.write(time.strftime('%X %x %Z') + '\n--------------------------------------------------------------\n')
-    for dataset in data:
-        if isinstance(channels, int):
-            fp.write(str(dataset) + '\n')
-        else:
-            j = 0
-            j = range(0,len(channels))
-            for k in j:
-                if k == max(j):
-                    fp.write(str(dataset[k]) + '\n')
-                else:
-                    fp.write(str(dataset[k]) + ',')
-    print 'Data logged to ' + filename + str(rectime) + '.txt' + '\n'
+class QuickSample:
+    
+    def __init__(self, filename, sampleRate, sampleDuration, testDuration, channels):
+        '''Default constructor block
 
-def main(filename, sampleRate, duration, channels):
-    '''
-    Run the main log for the data logging application
-    '''
-    i = 0
-    while i < 8:
-        rectime = time.clock()
-        data = sampleRun(duration, sampleRate, channels)
+        '''
+        self.filename = filename
+        self.sampleRate = sampleRate
+        self.sampleDuration = sampleDuration
+        self.testDuration = testDuration
+        self.channels = channels
+
+    def sampleRun(self):
+        '''
+        Sample from the ADC and log into an array
+        '''
+        # Initialise the ADC using the default mode
+        # For now only use one ADC, so only create one ADC object
+        adc = ADS1x15(address=ADC_ADDRESS_1, ic=ADS1115)
+        samples = range(0, ((self.sampleDuration*self.sampleRate)-1))
+        volts = [None] * len(samples)  # Initialize the array to print to
+        for sample in samples:
+            if not isinstance(self.channels, int):
+                volts[sample] = [adc.readADCSingleEnded(self.channels[0], 3300, self.sampleRate) / 1000,
+                                 adc.readADCSingleEnded(self.channels[1], 3300, self.sampleRate) / 1000,
+                                 adc.readADCSingleEnded(self.channels[2], 3300, self.sampleRate) / 1000]
+            else:
+                volts[sample] = adc.readADCSingleEnded(self.channels, 3300, SAMPLE_RATE) / 1000
+        return volts
+
+    def log(self, rectime, data):
+        '''Log the data collected in an array to a file to be used for later
+        '''
+        fp = open(self.filename + '-' + str(rectime) + '.txt', 'w+')
+        fp.write('ADS 1115 Data Collection\nSample Rate: ' + str(self.sampleRate) + ' Herz\nDuration: ' + str(self.sampleDuration) + ' Seconds\n')
+        fp.write(time.strftime('%X %x %Z') + '\n--------------------------------------------------------------\n')
+        for dataset in data:
+            if isinstance(self.channels, int):
+                fp.write(str(dataset) + '\n')
+            else:
+                j = 0
+                j = range(0,len(self.channels))
+                for k in j:
+                    if k == max(j):
+                        fp.write(str(dataset[k]) + '\n')
+                    else:
+                        fp.write(str(dataset[k]) + ',')
+        print 'Data logged to ' + self.filename + str(rectime) + '.txt' + '\n'
+
+    def mainLoop(self):
+        '''Run the main log for the data logging application
+        '''
+        print "Data collection initiated. Press Ctrl+C to cancel"
+        i = 0
+        while i < 8:
+            rectime = time.clock()
+            data = self.sampleRun()
+            self.log(rectime, data)
+            time.sleep(self.testDuration)  # Sleep for 15 minutes
+            i += 1
         print 'Finished data collection. Logging...\n'
-        log(filename, data, sampleRate, duration, channels, rectime)
-        time.sleep(60*15)  # Sleep for 15 minutes
-        i += 1
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-    main(LOGFILE_NAME, SAMPLE_RATE, DURATION, CHANNELS)
+    q = QuickSample(LOGFILE_NAME, SAMPLE_RATE, SAMPLE_DURATION, TEST_DURATION, CHANNELS)
+    q.mainLoop()
     print 'Data logging successful. Exiting...\n'
