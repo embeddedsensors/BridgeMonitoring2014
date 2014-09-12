2b44de246b821c12c40569a4a7835529f55bd0e3
Matthew Iannucci <mpiannucci@gmail.com>
2014-09-12 11:36:05 -0400

    Update installBBBDependencies.sh

--- scripts/installBBBDependencies.sh
+++ scripts/installBBBDependencies.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# System upgrade
 sudo apt-get update && sudo apt-get upgrade
 
+# Install the device tree compiler
 wget -c https://raw.github.com/RobertCNelson/tools/master/pkgs/dtc.sh
 chmod +x dtc.sh
 ./dtc.sh
 
+# GPS tools
 sudo apt-get install -y python-pip pps-tools gpsd gpsd-clients ntp
 
+# Adafruit libraries
 sudo pip install Adafruit_BBIO
