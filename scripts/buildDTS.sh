85436d19036874f091425583a6176fe465228cc6
Matthew Iannucci <mpiannucci@gmail.com>
2014-09-12 11:35:18 -0400

    fix path again

--- scripts/buildDTS.sh
+++ scripts/buildDTS.sh
@@ -7,6 +7,6 @@ It uses the Device Tree Compiler to create dtbo files to set the firmware so we 
 Device Tree Files are all stored in the firmware directory
 '''
 
-cd EmbeddedStructuralHealthMonitoring/firmware/
+cd ../firmware/
 dtc -O dtb -o gps-ntp-00A0.dtbo -b 0 -@ gps-ntp.dts
 mv gps-ntp-00A0.dtbo /lib/firmware/
